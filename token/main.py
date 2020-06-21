203c628d31f7a0f4a63c0cbfba8fb3878812c583
lliutianc <lliuitanc@gmail.com>
2020-06-21 00:50:03 -0400

    fix import error

--- token/main.py
+++ token/main.py
@@ -2,7 +2,7 @@ import os, sys
 
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
-sys.path.append(curPath)
+sys.path.append(rootPath)
 
 import torch
 import numpy as np
