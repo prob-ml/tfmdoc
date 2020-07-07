4c1698b3f27146e9a0c3d96848573ebf3c68984c
Tianci LIU <lliutianc@gmail.com>
2020-07-06 22:19:55 -0400

    work version

--- merge_field.py
+++ merge_field.py
@@ -3,19 +3,24 @@ import numpy as np
 
 from pathlib import Path
 import os
+import shutil
 import logging
 import argparse
 
 
-from utils import get_logger
+from utils import get_logger, makedirs
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dev', action='store_true')
+parser.add_argument('--force_new', action='store_true')
 args = parser.parse_args()
 
 
 data_path = '/home/liutianc/emr-data'
 result_path = os.path.join(data_path, 'merge')
+if os.path.exists(result_path) and args.force_new:
+    shutil.rmtree(result_path)
+makedirs(result_path)
 
 job = os.path.basename(__file__)
 job = job.split('.')[0]
@@ -48,14 +53,14 @@ if __name__ == '__main__':
 	        logger.info(f'Start: {file}, group: {group}.')
 	        sub_diag = diag[diag['PatGroup'] == group]
 	        sub_diag_merged = sub_diag.groupby(['Patid', 'Fst_Dt'])['DiagId'].apply(lambda x: ' '.join(x))
-	        sub_diag_merged_df = sub_diag_merged.reset_index()
+	        sub_diag_merged_df = sub_diag_merged.to_frame().reset_index()
 	        
-	        sub_diag_merged_df.rename({'Patid': 'patid', 'Fst_Dt': 'date', 'DiagId': 'diags'})
+	        sub_diag_merged_df.rename(columns={'Patid': 'patid', 'Fst_Dt': 'date', 'DiagId': 'diags'}, inplace=True)
 	        to_write = os.path.join(result_path, 'diag_' + group + '.csv')
 	        if os.path.exists(to_write):
-	            sub_diag_merged.to_csv(to_write, mode='a', header=False, index=False)
+	            sub_diag_merged_df.to_csv(to_write, mode='a', header=False, index=False)
 	        else:
-	            sub_diag_merged.to_csv(to_write, index=False)
+	            sub_diag_merged_df.to_csv(to_write, index=False)
 	        
 	        logger.info(f'Finish: {file}, group: {group}.')
 
@@ -70,10 +75,10 @@ if __name__ == '__main__':
 	        logger.info(f'Start: {file}, group: {group}.')
 	        sub_proc = proc[proc['PatGroup'] == group]
 	        sub_proc_merged = sub_proc.groupby(['Patid', 'Fst_Dt'])['ProcId'].apply(lambda x: ' '.join(x))
-	        sub_proc_merged_df = sub_proc_merged.reset_index()
+	        sub_proc_merged_df = sub_proc_merged.to_frame().reset_index()
 	        
-	        sub_proc_merged_df.rename({'Patid': 'patid', 'Fst_Dt': 'date', 'ProcId': 'procs'})
-
+	        sub_proc_merged_df.rename(columns={'Patid': 'patid', 'Fst_Dt': 'date', 'ProcId': 'procs'}, inplace=True)
+	        
 	        to_write = os.path.join(result_path, 'proc_' + group + '.csv')
 	        if os.path.exists(to_write):
 	            sub_proc_merged_df.to_csv(to_write, mode='a', header=False, index=False)
@@ -83,17 +88,17 @@ if __name__ == '__main__':
 
 
 	for file in pharms:
-		print(file)
-	    pharm = pd.read_csv(file, sep=',', dtype = {'Patid': str})
+	    print(file)
+	    pharm = pd.read_csv(file, sep=',', dtype = {'Patid': str}, error_bad_lines=False)
 	    pharm = pharm.assign(PatGroup = pharm['Patid'].apply(lambda x: x[-1]))
 	    
 	    for group in userGroup:
 	        logger.info(f'Start: {file}, group: {group}.')
 	        sub_pharm = pharm[pharm['PatGroup'] == group]
 	        sub_pharm_merged = sub_pharm.groupby(['Patid', 'Fill_Dt'])['Gnrc_Nm'].apply(lambda x: ' '.join(x))
-	        sub_pharm_merged_df = sub_pharm_merged.reset_index()
-	        sub_pharm_merged_df.rename({'Patid': 'patid', 'Fill_Dt': 'date', 'Gnrc_Nm': 'drugs'})
-
+	        sub_pharm_merged_df = sub_pharm_merged.to_frame().reset_index()
+	        sub_pharm_merged_df.rename(columns={'Patid': 'patid', 'Fill_Dt': 'date', 'Gnrc_Nm': 'drugs'}, inplace=True)
+	        
 	        
 	        to_write = os.path.join(result_path, 'pharm_' + group + '.csv')
 	        if os.path.exists(to_write):
