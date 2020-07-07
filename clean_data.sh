c885e7ac0544f2c98545302a1f337bbbe303d087
Tianci LIU <lliutianc@gmail.com>
2020-07-06 22:39:59 -0400

    fix bugs

--- clean_data.sh
+++ clean_data.sh
@@ -7,21 +7,21 @@ do
 	diag='diag_201'$i'.csv'
 	echo "Start: "$diag
 # 	sed "s/\\.0//g" $INPATH$diag | cut -d "," -f 1,3-7,11 | sed "s/-//g" | sort  -t ',' -k 1n -k 7n -k 4n > $OUTPATH$diag
-    sed "s/\\.0//g" $INPATH$diag | cut -d "," -f 1,3-7,11 | sort  -t ',' -k 1n -k 7n -k 4n > $OUTPATH$diag
+    sed "s/\\.0//g" $INPATH$diag | cut -d "," -f 1,3-7,11 | sort  -t ',' -k 1n -k 7 -k 4n > $OUTPATH$diag
 	echo "Finish: "$diag
     
     # Clean pharm data.
     pharm='pharm_201'$i'.csv'
     echo "Start: "$pharm
 #     sed "s/\\.0//g" $INPATH$pharm | cut -d "," -f 1,8,15,20,26,27 | sed 's/-//g' | sed "s/\"//g" | sort -t ',' -k 1n -k 3n -k 2 > $OUTPATH$pharm
-    sed "s/\\.0//g" $INPATH$pharm | cut -d "," -f 1,8,15,20,26,27 | sed "s/\"//g" | sort -t ',' -k 1n -k 3n -k 2 > $OUTPATH$pharm
+    sed "s/\\.0//g" $INPATH$pharm | cut -d "," -f 1,8,15,20,26,27 | sed "s/\"//g" | sort -t ',' -k 1n -k 3 > $OUTPATH$pharm
     echo "Finish: "$pharm
     
     # Clean procedure data
     proc='proc_201'$i'.csv'
     echo "Start: "$proc
 #     sed "s/\\.0//g" $INPATH$proc | cut -d "," -f 1,3-6,9 | sed 's/-//g' | sort -t ',' -k 1n -k 6n -k 5n > $OUTPATH$proc
-    sed "s/\\.0//g" $INPATH$proc | cut -d "," -f 1,3-6,9 | sort -t ',' -k 1n -k 6n -k 5n > $OUTPATH$proc
+    sed "s/\\.0//g" $INPATH$proc | cut -d "," -f 1,3-6,9 | sort -t ',' -k 1n -k 6 -k 5n > $OUTPATH$proc
     echo "Finish: "$proc    
 done
 
