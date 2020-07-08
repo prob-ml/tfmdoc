3cf13300ed4637df945c7641d8ad37048286e076
Tianci LIU <lliutianc@gmail.com>
2020-07-07 22:29:45 -0400

    finish whole pipeline for data cleaning.

---
+++ data_main.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# echo $*
+select='n'
+field='n'
+merge='n'
+clean='n'
+
+
+INPATH='/nfs/turbo/lsa-regier/OPTUMInsight_csv/'
+# OUTPATH='/nfs/turbo/lsa-regier/emr-data/'
+OUTPATH='/home/liutianc/emr-data/'
+
+para_num=$#
+
+usage() {
+    echo -e " -s 'select data: [y/n]' \n -f 'create user-date-field data [y/n]' \n -m 'create user-seq data [y/n]' \n -c 'clean tmp files [y/n]' \n" 1>&2
+#     exit 1
+}
+
+if [[ ${para_num} -lt 1 ]]; then
+    echo "None of expected manipulation is specified: "
+    usage
+fi
+
+while getopts ":s:f:m:c:" opt
+do
+    case $opt in
+        s)
+        select=$OPTARG;;
+        f)
+        field=$OPTARG;;
+        m)
+        merge=$OPTARG;;
+        c)
+        clean=$OPTARG;;
+        ?)
+        error=$OPTARG
+        echo "Unknown parameter: ${error}, expect: "
+        usage
+        exit 1;;
+
+    esac
+done
+
+if [[ ${select} == 'y' ]]; then
+    echo "Start: select column from original data. "
+    ./data_select.sh $INPATH $OUTPATH 
+    echo "Finish: select column from original data. "
+fi
+
+if [[ ${field} == 'y' ]]; then
+    echo "Start: create user-date-field data. "
+    # NOTE: after making sure everything is fine, remove --dev here.
+    python3 ./data_field.py --create_field_seq --dev --merge_field --path $OUTPATH 1>&2
+    echo "Finish: create user-date-field data. "
+fi 
+
+if [[ ${merge} == 'y' ]]; then
+    echo "Start: create user-seq data. "
+    ./data_merge.sh $OUTPATH
+    python3 ./data_merge.py --path $OUTPATH 1>&2
+    echo "Finish: create user-seq data. "
+fi 
+
+if [[ ${clean} == 'y' ]]; then
+    echo "Start: clean tmp files. "
+    for (( year = 0; year <= 8; year ++ ));
+    do
+        for (( group = 0; group <= 9; group++ ));
+        do
+            rm $OUTPATH'201'$year'_'$group'.csv'
+            rm $OUTPATH'diag_201'$year'_'$group'.csv'
+            rm $OUTPATH'pharm_201'$year'_'$group'.csv'
+            rm $OUTPATH'proc_201'$year'_'$group'.csv'
+        done
+    done
+    echo "Finish: clean tmp files. "
+fi 
+
+echo "Finish all..."
---
+++ data_merge.py
@@ -0,0 +1,50 @@
+import os
+import time
+import argparse
+
+from utils import get_logger, makedirs
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', type=str)
+    args = parser.parse_args()
+    
+    job = os.path.basename(__file__)
+    job = job.split('.')[0]
+    logger = get_logger(job)
+    
+    path = args.path
+
+    user_group = [str(i) for i in range(10)]
+    start = time.time()
+    for group in user_group:
+        
+        read = os.path.join(path, f'group_{group}.csv')
+        write = os.path.join(path, f'group_{group}_merged.csv')
+        
+        with open(read, 'r') as infile:
+            with open(write, 'w') as outfile:
+                outfile.write('patid,document\n')
+                current_user = ''
+                current_document = ''
+                for line in infile:
+                    user, date, hist = line.split(",")
+                    hist = hist.strip()
+                    
+                    # first record of current user
+                    if current_user == '':
+                        current_user = user
+                        current_document += hist
+                    # Same user: just append
+                    elif current_user == user:
+                        current_document += ' [SEP] ' + hist
+                    # next user
+                    else:
+                        outfile.write(current_user + ',' + current_document + '\n')
+                
+        logger.info(f'Finish: group: {group}, time cost: {round(time.time() - start, 2)}.')
+        start = time.time()
+    
+    logger.info(f'Finish all...')
+                        
+                        
