9ff34d09b32c3e1c362e60b47aabafa5cb0c2c6d
lliutianc <lliutianc@gmail.com>
2020-07-08 15:13:28 -0400

    Update utils.py

--- data/utils.py
+++ data/utils.py
@@ -4,10 +4,11 @@ import os
 
 def get_logger(job):
     """from github: ffjord"""
-    log_path = '/home/liutianc/emr/logs'
+#     log_path = '/home/liutianc/emr/logs'
     cur_path = os.path.abspath(os.path.dirname(__file__))
-    print(cur_path)
-    exit(1)
+    root_path = os.path.split(cur_path)[0]
+    log_path += os.path.join(root_path, 'logs')
+    print(log_path)
     makedirs(log_path)
 
     log_path += '/' + job
