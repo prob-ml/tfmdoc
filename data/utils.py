ebfc2b591efc31484bf242db0f8a9be030e34987
lliutianc <lliutianc@gmail.com>
2020-07-08 15:14:05 -0400

    Update utils.py

--- data/utils.py
+++ data/utils.py
@@ -7,8 +7,9 @@ def get_logger(job):
 #     log_path = '/home/liutianc/emr/logs'
     cur_path = os.path.abspath(os.path.dirname(__file__))
     root_path = os.path.split(cur_path)[0]
-    log_path += os.path.join(root_path, 'logs')
-    print(log_path)
+    log_path = os.path.join(root_path, 'logs')
+    print('path:', log_path)
+    exit(1)
     makedirs(log_path)
 
     log_path += '/' + job
