3beb7b85351bf102fcf4fda9f56e92efdfa869bf
lliutianc <lliutianc@gmail.com>
2020-07-08 15:14:37 -0400

    fix directory bug

--- data/utils.py
+++ data/utils.py
@@ -8,8 +8,6 @@ def get_logger(job):
     cur_path = os.path.abspath(os.path.dirname(__file__))
     root_path = os.path.split(cur_path)[0]
     log_path = os.path.join(root_path, 'logs')
-    print('path:', log_path)
-    exit(1)
     makedirs(log_path)
 
     log_path += '/' + job
