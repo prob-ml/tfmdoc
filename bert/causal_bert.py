2ba53e82747d9a57073d349b3125db896f9a7d00
Tianci LIU <lliutianc@gmail.com>
2020-08-06 02:12:47 -0400

    demo for causal bert

---
+++ bert/causal_bert.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import argparse
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
+import numpy as np
+import torch
+import pandas as pd
+import random
+from torch.distributions.binomial import Binomial
+from torch.distributions.bernoulli import Bernoulli
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+from transformers import DataCollatorForLanguageModeling, BertForMaskedLM
+from transformers import Trainer, TrainingArguments
+
+from tokens import WordLevelBertTokenizer
+from vocab import create_vocab
+from data import CausalBertDataset, MLMDataset
+
+
+class CausalBert(nn.Module):
+    def __init__(self, token_embed, hidden_size=256, binary_response=True, learnable_docu_embed=False):
+        super().__init__()
+        self.token_embed = token_embed
+
+        embed_out = self.token_embed.weight.shape[1]
+
+        # G head: logit-linear mapping
+        self.g = nn.Sequential(*[nn.Linear(embed_out, 1), nn.Sigmoid()])
+
+        # Q_1 and Q_0 heads: two-hidden layers mapping.
+        if binary_response:
+            self.q1 = nn.Sequential(
+                *[nn.Linear(embed_out, hidden_size), nn.ReLU(inplace=True), nn.Linear(hidden_size, hidden_size),
+                    nn.ReLU(inplace=True), nn.Linear(hidden_size, 1), nn.Sigmoid()])
+            self.q0 = nn.Sequential(
+                *[nn.Linear(embed_out, hidden_size), nn.ReLU(inplace=True), nn.Linear(hidden_size, hidden_size),
+                    nn.ReLU(inplace=True), nn.Linear(hidden_size, 1), nn.Sigmoid()])
+        else:
+            self.q1 = nn.Sequential(
+                *[nn.Linear(embed_out, hidden_size), nn.ReLU(inplace=True), nn.Linear(hidden_size, hidden_size),
+                    nn.ReLU(inplace=True), nn.Linear(hidden_size, 1), ])
+            self.q0 = nn.Sequential(
+                *[nn.Linear(embed_out, hidden_size), nn.ReLU(inplace=True), nn.Linear(hidden_size, hidden_size),
+                    nn.ReLU(inplace=True), nn.Linear(hidden_size, 1), nn.Sigmoid()])
+
+    def forward(self, tokens):
+        embed_token = self.token_embed(tokens)
+        embed_docu = torch.sum(embed_token, axis=1)
+
+        return self.g(embed_docu), self.q1(embed_docu), self.q0(embed_docu)
+
+
+def causal_bert_task(args):
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type=str, choices=['daily', 'merged'], default='merged')
+    parser.add_argument('--diag', type=str, choices=['uni', 'raw'], default='uni',
+                        help='Which tokens to use: uni for Uni-diag code, and raw for raw data.')
+
+    # parser.add_argument('--create-vocab', action='stro, choices=['daily', 'merged'], default='merged')
+    parser.add_argument('--truncate', type=str, choices=['first', 'last', 'random'], default='first')
+    parser.add_argument('--min-length', type=int, default=10, help='Min length of a sequence to be used in Bert')
+    parser.add_argument('--max-length', type=int, default=512, help='Max length of a sequence used in Bert')
+    parser.add_argument('--bsz', type=int, default=8, help='Batch size in training')
+    parser.add_argument('--epochs', type=int, default=10, help='Epoch in production version')
+    parser.add_argument('--force-new', action='store_true', default=False, help='Force to train a new MLM.')
+    parser.add_argument('--model', type=str, choices=['dev', 'behrt', 'med-bert'], default='behrt',
+                        help='Which bert to use')
+    parser.add_argument('--cuda', type=str, help='Visible CUDA to the task.')
+
+    parser.add_argument('--no-eval', action='store_true', default=False,
+                        help='Do not evaluate during training.')
+
+    args = parser.parse_args()
+
+    args.unidiag = (args.diag == 'uni')
+    args.eval_when_train = not args.no_eval
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda
+    print(f'Prepare: check process on cuda: {args.cuda}...')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    result_path = os.path.join(curPath, 'results', args.model, 'MLM', args.data, 'unidiag' if args.unidiag else 'original' )
+    trained_model = os.path.join(curPath, 'trained', args.model, 'MLM', 'unidiag' if args.unidiag else 'original')
+    make_dirs(result_path, trained_model)
+    print(f'Prepare: check result at {result_path}.')
+
+    assert args.model in ['dev', 'behrt', 'med-bert'], f'Not supported for model config: {args.model} yet...'
+    if args.model == 'med-bert':
+        raise UserWarning('Configuration of Med-Bert from the paper is still mysterious, '
+                          'the final result may be unexpected...')
+    mlm_task(args)
+
+    print('Finish all...')
--- bert/data.py
+++ bert/data.py
@@ -2,14 +2,17 @@ import os
 from typing import Any, List
 import random
 
+import numpy as np
+
 import torch
+from torch.distributions.binomial import Binomial
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizer
 
 from utils import DATA_PATH
 
 
-class LineByLineTextDataset(Dataset):
+class MLMDataset(Dataset):
     """
     This will be superseded by a framework-agnostic approach soon.
     """
@@ -37,6 +40,7 @@ class LineByLineTextDataset(Dataset):
                     lines += [line.replace('\n', '').split(',')[1] for line in f.read().splitlines() if
                               (len(line) > 0 and not line.isspace())][1:] #[1:] drop HEADER row.
         truncated_lines = []
+        
         for line in lines:
             token_list = line.split(' ')
             if len(token_list) <= min_length:
@@ -52,10 +56,10 @@ class LineByLineTextDataset(Dataset):
                 elif truncate_method == 'random':
                     token_idx = random.sample(range(len(token_list)), max_length)
                     token_list = [token_list[idx] for idx in token_idx.sort()]
-
+                
                 truncated_lines.append(' '.join(token_list))
         lines = truncated_lines
-
+        # Padding is handled by MLMCollator.
         batch_encoding = tokenizer.batch_encode_plus(lines, add_special_tokens=add_special_tokens,
                                                      max_length=max_length, truncation=True)
         self.examples = batch_encoding["input_ids"]
@@ -87,4 +91,118 @@ class LineByLineTextDataset(Dataset):
 
         return False
 
+
+class CausalBertDataset(Dataset):
+    """
+    This will be superseded by a framework-agnostic approach soon.
+    """
+    def __init__(self,
+                 tokenizer: PreTrainedTokenizer,
+                 data_type: str,
+                 is_unidiag: bool,
+                 max_length: int,
+                 min_length: int,
+                 device: str,
+                 group: list = None,
+                 add_special_tokens: bool = True,
+                 truncate_method: str = 'first'):
+
+        if group is None: group = range(10)
+        self.user_group = [str(i) for i in group]# if group is None else [str(group)]
+        self.data_type = data_type
+        self.is_unidiag = is_unidiag
+        self.device = device
+        lines = []
+        for file in os.listdir(DATA_PATH):
+            if self.is_target(file):
+                with open(os.path.join(DATA_PATH, file), encoding='utf-8') as f:
+                    # f.read().splitlines() will drop the '\n' at the end of each line automatically.
+                    lines += [line.replace('\n', '').split(',')[1] for line in f.read().splitlines() if
+                              (len(line) > 0 and not line.isspace())][1:] #[1:] drop HEADER row.
+
+        truncated_lines = []
+        prop_score = []
+        for line in lines:
+            token_list = line.split(' ')
+            
+            # Drop too short sequence.
+            if len(token_list) <= min_length:
+                continue
+                
+            # Sample from too long sequence.
+            if len(token_list) > max_length:
+                if truncate_method == 'first':
+                    token_list = token_list[:max_length]
+                elif truncate_method == 'last':
+                    token_list = token_list[-max_length:]
+                elif truncate_method == 'random':
+                    token_idx = random.sample(range(len(token_list)), max_length)
+                    token_list = [token_list[idx] for idx in token_idx.sort()]
+                line = ' '.join(token_list)
+                
+            truncated_lines.append(line)
+            prop_score.append(self.treatment_portion(line))
+        lines = truncated_lines
+        batch_encoding = tokenizer.batch_encode_plus(lines, add_special_tokens=add_special_tokens,
+                                                     max_length=max_length, truncation=True, padding=True)
+        self.tokens = batch_encoding["input_ids"]
+
+        # Create propensity score, treatment and response
+        self.prop_score = torch.tensor(prop_score, dtype=torch.float32)
+        self.treatment = Binomial(1, self.prop_score).sample()
+        self.response = self.generate_outcome(self.treatment, self.prop_score)
+
+        self.prop_score = self.prop_score.reshape(-1, 1).to(self.device)
+        self.treatment = self.treatment.reshape(-1, 1).to(self.device)
+        self.response = self.response.reshape(-1, 1).to(self.device)
+        
+    def __len__(self) -> int:
+        return len(self.tokens)
+
+    def __getitem__(self, i) -> list:
+        token = torch.tensor(self.tokens[i], dtype=torch.long, device=self.device)
+        treatment = self.treatment[i]
+        response = self.response[i]
+        return token, treatment, response
+
+    def treatment_portion(self, x):
+        x = x.replace(' [SEP] ', '').strip()
+        token_list = x.split(' ')
+        treat_cnt = 0
+        if len(token_list) > 0:
+            for token in token_list:
+                if 'diag' not in token:
+                    treat_cnt += 1
+
+        score = (treat_cnt + 1e-8)/ (len(token_list) + 1e-8)
+        score = max(score, 0.1)
+        score = min(score, 0.9)
+
+        return score
+
+    def generate_outcome(self, treatment, prop_score, alpha=0.25, beta=1., c=0.2):
+        prob = torch.sigmoid(alpha * treatment + beta * (prop_score - c))
+
+        return Binomial(1, prob).sample()
+    
+    def is_target(self, file):
+
+        # First check if 'unidiag' is correct.
+        if self.is_unidiag:
+            if 'unidiag' not in file:
+                return False
+        else:
+            if 'unidiag' in file:
+                return False
+
+        # Since a file name may contain 'merged', but will not contain 'daily',
+        # so we need to use nested if-condition.
+        if 'merged' in file:
+            if self.data_type == 'merged':
+                return max(map(lambda x: x in file, self.user_group))
+        else:
+            if self.data_type != 'merged':
+                return max(map(lambda x: x in file, self.user_group))
+
+        return False
 
