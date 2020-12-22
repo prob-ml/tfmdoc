978a3aa4d0e44d46422df62e64dd4d7cc4695563
lliutianc <lliuitanc@gmail.com>
2020-12-22 18:03:57 -0500

    set drug_name to data

--- vte/select_pharm_antico.sh
+++ vte/select_pharm_antico.sh
@@ -6,7 +6,7 @@ OUTFILE=$OUTPATH'pharm_antico.csv'
 
 # Write head
 pharm='pharm_2010.csv'
-sed "s/\\.0//g" $INPATH$pharm | cut -d "," -f 1,8,15,20,26,27| sed 's/ /_/g' | sed "s/\"//g" | head -1 > $OUTFILE
+sed "s/\\.0//g" $INPATH$pharm | cut -d "," -f 1,5,8,15,20,26,27| sed 's/ /_/g' | sed "s/\"//g" | head -1 > $OUTFILE
 
 for ((year = 0; year <= 8; year++));
 do
@@ -14,6 +14,6 @@ do
     echo "Start: "$pharm
     for pharm_drug in `cat pharm_antico`
     do
-    	sed "s/\\.0//g" $INPATH$pharm | cut -d "," -f 1,8,15,20,26,27 | egrep -i $pharm_drug | sed 's/ /_/g' | sed "s/\"//g" >> $OUTFILE
+    	sed "s/\\.0//g" $INPATH$pharm | cut -d "," -f 1,5,8,15,20,26,27 | egrep -i $pharm_drug | sed 's/ /_/g' | sed "s/\"//g" >> $OUTFILE
     done
 done
