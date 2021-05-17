9e404c64158104ef32527a5af2f33e5517ff2f11
chathasphere <pgchatha@gmail.com>
2021-05-17 18:09:07 -0400

    clean up trainer placeholder

--- ehr/trainer.py
+++ ehr/trainer.py
@@ -1,7 +1,4 @@
-import time
-
 import torch
-from torch import nn
 
 
 class Trainer:
@@ -13,13 +10,12 @@ class Trainer:
         self.padding_ix = padding_ix
 
     def train(self, training_data, epochs):
-        training_start_time = time.time()
 
         if torch.cuda.is_available():
             self.model.cuda()
 
         training_losses = []
-        loss_fn = nn.CrossEntropyLoss(ignore_index=self.padding_ix)
+        loss_fn = torch.nn.CrossEntropyLoss(ignore_index=self.padding_ix)
         features, labels = training_data
         features = torch.from_numpy(features)
         labels = torch.from_numpy(labels)
@@ -27,20 +23,22 @@ class Trainer:
             features = features.cuda()
             labels = labels.cuda()
         n_sequences = features.shape[0]
-        for epoch in range(epochs):
+        for _ in range(epochs):
             for i in range(0, n_sequences, self.batch_size):
                 if i + self.batch_size > n_sequences:
                     continue
-                X = features[i : i + self.batch_size, :]
+                x = features[i : i + self.batch_size, :]
                 y = labels[i : i + self.batch_size]
-                y_hat = self.model(X)
+                y_hat = self.model(x)
                 loss = loss_fn(y_hat, y)
 
                 self.model.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-                training_loss = loss.item()
-                print(training_loss)
-                training_losses.append(training_loss)
+                training_losses.append(loss.item())
 
         return training_losses
+
+    def __str__(self):
+        # placeholder until we start using lightning
+        return self.__class__.__name__
