a2cbd79cae4091aafc3c45e6f326164327ae0d15
chathasphere <pgchatha@gmail.com>
2021-08-05 17:16:25 -0400

    Write out patient info in chunks

--- tfmdoc/preprocess.py
+++ tfmdoc/preprocess.py
@@ -1,8 +1,8 @@
 import logging
 import os
 
+import h5py
 import numpy as np
-import pandas as pd
 from fastparquet import ParquetFile
 
 from tfmdoc.chunk_iterator import chunks_of_patients
@@ -31,38 +31,32 @@ def claims_pipeline(
 
     disease_codes = [f"{code: <7}".encode("utf-8") for code in disease_codes]
 
-    offsets, records, labels = extract_patient_info(
-        diags, disease_codes, length_range, n_processed
-    )
+    os.chdir(OUTPUT_DIR)
 
-    save_output_files(offsets, records, labels)
+    with h5py.File("preprocessed.hdf5", "a") as f:
+        names = (
+            "patient_offsets",
+            "diag_records",
+            "patient_labels",
+            "patient_ids",
+        )
+        for name in names:
+            f.create_dataset(name, maxshape=(None,))
+
+        process_chunks(diags, disease_codes, length_range, n_processed, f)
+
+        records = f["diag_records"]
+        # assign each diag code a unique integer key
+        code_lookup, indexed_records = np.unique(records, return_inverse=True)
+        # make sure that zero does not map to a code
+        code_lookup = np.insert(code_lookup, 0, "pad")
+        indexed_records += 1
+        f.create_dataset("patient_tokens", data=indexed_records)
+        f.create_dataset("diag_code_lookup", data=code_lookup)
+
     os.chdir(owd)
 
     log.info("Completed pipeline")
-
-
-def save_output_files(offsets, records, labels):
-    offsets = pd.concat(offsets)
-    patient_ids = offsets.index.to_numpy()
-    offsets = np.cumsum(offsets.to_numpy())
-    records = pd.concat(records).to_numpy()
-    patient_labels = pd.concat(labels).to_numpy()
-    # assign each diag code a unique integer key
-    # this might be computationally taxing
-    code_lookup, indexed_records = np.unique(records, return_inverse=True)
-    # make sure that zero does not map to a code
-    code_lookup = np.insert(code_lookup, 0, "pad")
-    indexed_records += 1
-
-    # write out
-    if not os.path.exists(OUTPUT_DIR):
-        os.mkdir(OUTPUT_DIR)
-
-    np.save(OUTPUT_DIR + "patient_offsets", offsets)
-    np.save(OUTPUT_DIR + "patient_ids", patient_ids)
-    np.save(OUTPUT_DIR + "diag_code_lookup", code_lookup)
-    np.save(OUTPUT_DIR + "diag_records", indexed_records)
-    np.save(OUTPUT_DIR + "patient_labels", patient_labels)
 
 
 def transform_patients_chunk(chunk, disease_codes):
@@ -76,20 +70,37 @@ def transform_patients_chunk(chunk, disease_codes):
     chunk["DiagId"] = chunk["Icd_Flag"] + b":" + chunk["Diag"]
     chunk.drop(columns=["Icd_Flag", "Diag"], inplace=True)
     # split up codes
-    chunk["DiagId"] = chunk["DiagId"].apply(lambda x: (x[:5], x[:6], x))
+    chunk["DiagId"] = chunk["DiagId"].apply(lambda x: (x[:5], x[:6], x.rstrip()))
     # be careful as this will change the column naming within the generator!
     chunk.rename(columns={"Patid": "patid", "DiagId": "diag"}, inplace=True)
     # data go boom!
     return chunk.explode("diag")
 
 
-def extract_patient_info(diags, disease_codes, length_range, n_processed):
-    records = []
-    labels = []
-    offsets = []
+def pull_patient_info(chunk, length_range):
+    chunk_info = {}
+    # if a patient has any code associated with the disease, flag as a positive
+    labeled_ids = chunk.groupby("patid")["is_case"].any().astype(int)
+    # drop rows with the disease's diag code to prevent leakage
+    chunk = chunk[chunk["is_case"] == False]
+    counts = chunk.groupby("patid")["diag"].count().rename("count")
+    # drop patients with too few or too many records
+    # a relatively small number of patients might comprise a
+    # huge portion of the dataset due to extra-long (1k+) diag sequences
+    chunk_info["patient_offsets"] = counts[counts.between(*length_range)].to_numpy()
+    chunk = chunk.join(counts, on="patid", how="right")
+    chunk_info["diag_records"] = chunk["diag"].to_numpy()
+    labeled_ids = labeled_ids.to_frame().join(counts, on="patid", how="right")
+    chunk_info["patient_labels"] = labeled_ids["is_case"]
+    chunk_info["patient_ids"] = labeled_ids.index
 
+    return chunk_info
+
+
+def process_chunks(diags, disease_codes, length_range, n_processed, file):
     n_patients = 0
     n_records = 0
+    n_chunks = 0
 
     for chunk in chunks_of_patients(diags, ("Patid", "Icd_Flag", "Diag", "Fst_Dt")):
         # break out of loop if there's an empty chunk
@@ -97,25 +108,16 @@ def extract_patient_info(diags, disease_codes, length_range, n_processed):
             log.warning("Empty chunk returned!")
             continue
         chunk = transform_patients_chunk(chunk, disease_codes)
-        # if a patient has any code associated with the disease, flag as a positive
-        labeled_ids = chunk.groupby("patid")["is_case"].any().astype(int)
-        # drop rows with the disease's diag code to prevent leakage
-        chunk = chunk[chunk["is_case"] == False]
-        counts = chunk.groupby("patid")["diag"].count().rename("count")
-        # drop patients with too few or too many records
-        # a relatively small number of patients might comprise a
-        # huge portion of the dataset due to extra-long (1k+) diag sequences
-        counts = counts[counts.between(*length_range)]
-        offsets.append(counts)
-        chunk = chunk.join(counts, on="patid", how="right")
-        records.append(chunk["diag"])
-        labeled_ids = labeled_ids.to_frame().join(counts, on="patid", how="right")
-        labels.append(labeled_ids["is_case"])
-        n_patients += len(counts)
+        chunk_info = pull_patient_info(chunk, length_range)
+        n_patients += len(chunk_info["patient_offsets"])
         n_records += len(chunk)
-        log.info(f"{n_patients :,} patient ids, {n_records :,} records processed")
-        if n_processed is not None and n_patients > n_processed:
+        log.info(f"Processed {n_patients :,} patient ids, {n_records :,} records")
+        for name, arr in chunk_info.items():
+            file[name].resize(len(file[name]) + len(arr), axis=0)
+            file[name][-len(arr) :] = arr
+            log.info(f"Patient info written for chunk {n_chunks}")
+
+        n_chunks += 1
+        if n_processed is not None and n_patients >= n_processed:
             # stop processing chunks if enough patient ids have been processed
             break
-
-    return offsets, records, labels
