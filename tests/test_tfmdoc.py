f9a1c97d562964851190bb579a8445abc5a7cfbc
chathasphere <pgchatha@gmail.com>
2022-03-17 10:39:37 -0400

    basic BERT test

--- tests/test_tfmdoc.py
+++ tests/test_tfmdoc.py
@@ -1,91 +1,8 @@
-import os
-
-import pytorch_lightning as pl
 import torch
 from hydra import compose, initialize
 from hydra.utils import instantiate
-from torch.utils.data import DataLoader
 
-from tfmdoc import load_data
 from tfmdoc.aipw import aipw_estimator
-from tfmdoc.preprocess import DiagnosisPipeline
-
-
-def test_lightning():
-
-    with initialize(config_path=".."):
-        cfg = compose(
-            config_name="config",
-            overrides=[
-                "model.d_model=32",
-                "model.n_blocks=1",
-                "disease_codes.ald=['7231']",
-                "preprocess.data_dir=tests/test_data/",
-                "preprocess.output_dir=tests/test_data/test_lightning/",
-                "preprocess.filename=preprocessed",
-            ],
-        )
-
-        assert cfg["model"]["n_blocks"] == 1
-
-        cpl = DiagnosisPipeline(
-            cfg.preprocess.data_dir,
-            cfg.preprocess.output_dir,
-            cfg.disease_codes.ald,
-            output_name=cfg.preprocess.filename,
-            test=True,
-        )
-        cpl.run()
-
-        preprocess_dir = "tests/test_data/test_lightning/"
-        dataset = load_data.ClaimsDataset(preprocess_dir)
-        train_loader = DataLoader(
-            dataset, collate_fn=load_data.padded_collate, batch_size=4
-        )
-        mapping = dataset.code_lookup
-        tfmd = instantiate(cfg.model, n_tokens=mapping.shape[0])
-        trainer = pl.Trainer(fast_dev_run=True)
-        trainer.fit(tfmd, train_loader)
-        os.remove(preprocess_dir + "preprocessed.hdf5")
-        os.rmdir(preprocess_dir)
-
-
-def test_early_etl():
-
-    with initialize(config_path=".."):
-        cfg = compose(
-            config_name="config",
-            overrides=[
-                "model.d_model=32",
-                "model.n_blocks=1",
-                "disease_codes.ald=['7231']",
-                "preprocess.data_dir=tests/test_data/",
-                "preprocess.output_dir=tests/test_data/test_lightning/",
-                "preprocess.filename=preprocessed",
-                "preprocess.mode=early_detection",
-            ],
-        )
-
-        cpl = DiagnosisPipeline(
-            cfg.preprocess.data_dir,
-            cfg.preprocess.output_dir,
-            cfg.disease_codes.ald,
-            mode=cfg.preprocess.mode,
-            output_name=cfg.preprocess.filename,
-            test=True,
-        )
-        cpl.run()
-
-        preprocess_dir = "tests/test_data/test_lightning/"
-        dataset = load_data.EarlyDetectionDataset(
-            preprocess_dir, late_cutoff=10, early_cutoff=30
-        )
-        assert len(dataset)
-        assert len(dataset[0]) == 2
-        assert len(dataset[0][0]) == 5
-        assert dataset[0][1][4] - dataset[0][0][4] == 1
-        os.remove(preprocess_dir + "preprocessed.hdf5")
-        os.rmdir(preprocess_dir)
 
 
 def test_model():
@@ -123,38 +40,6 @@ def test_bow():
         assert model.configure_optimizers().defaults
         assert model.training_step((t, v, w, x, y), 0) > 0
         assert model.validation_step((t, v, w, x, y), 0) > 0
-
-
-def test_pipeline():
-    with initialize(config_path=".."):
-        cfg = compose(
-            config_name="config",
-            overrides=[
-                "disease_codes.ald=['7231']",
-                "preprocess.data_dir=tests/test_data/",
-                "preprocess.output_dir=tests/test_data/test_pipeline/",
-                "preprocess.filename=preprocessed",
-            ],
-        )
-        cpl = DiagnosisPipeline(
-            cfg.preprocess.data_dir,
-            cfg.preprocess.output_dir,
-            cfg.disease_codes.ald,
-            output_name=cfg.preprocess.filename,
-            test=True,
-        )
-        cpl.run()
-        preprocess_dir = "tests/test_data/test_pipeline/"
-        torch_dataset = load_data.ClaimsDataset(preprocess_dir)
-        assert torch_dataset.offsets[-1] == torch_dataset.records.shape[0]
-        t, v, w, x, y = torch_dataset[7]
-        assert len(x) == torch_dataset.offsets[7] - torch_dataset.offsets[6]
-        assert y.item() in {0, 1}
-        assert w.shape[0] == 2
-        assert v.max().item() == 1
-        assert t.shape[0] == x.shape[0]
-        os.remove(preprocess_dir + "preprocessed.hdf5")
-        os.rmdir(preprocess_dir)
 
 
 def test_aipw():
