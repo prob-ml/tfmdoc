Fixed pretrain masking and loss

run.py
@@ -82,9 +82,10 @@ def main(cfg=None):
         mode=cfg.preprocess.mode,
     )
     mapping = dataset.code_lookup
-    # initialize tfmd model from config settings
-    mask = cfg.preprocess.mode == "pretraining"
-    tfmd = instantiate(cfg.model, mask=mask, n_tokens=mapping.shape[0])
+    if cfg.preprocess.mode == "pretraining":
+        model = instantiate(cfg.bert, n_tokens=mapping.shape[0])
+    else:
+        model = instantiate(cfg.model, n_tokens=mapping.shape[0])
     # ensure model with least validation loss is used for testing
     if val_size:
         callbacks = [ModelCheckpoint(monitor="val_loss")]
@@ -97,13 +98,13 @@ def main(cfg=None):
         callbacks=callbacks,
     )
     # train and validate
-    trainer.fit(tfmd, loaders["train"], loaders.get("val"))
+    trainer.fit(model, loaders["train"], loaders.get("val"))
     # test model
     trainer.test(test_dataloaders=loaders["test"], ckpt_path="best")
-    diagnostic_plot(tfmd, trainer)
+    diagnostic_plot(model, trainer)
     if cfg.train.save_prediction:
-        torch.save(tfmd.results[0], "test_predictions.pt")
-        torch.save(tfmd.results[1], "test_targets.pt")
+        torch.save(model.results[0], "test_predictions.pt")
+        torch.save(model.results[1], "test_targets.pt")
 
 
 def diagnostic_plot(model, trainer):

tfmdoc/datasets.py
@@ -36,14 +36,6 @@ class ClaimsDataset(Dataset):
         self.ages = torch.from_numpy(np.array(self.file["ages"]))
         self.ages = torch.clamp(self.ages, max=99)
         self._length = self.ids.shape[0]
-        # array of binary labels (is a patient a case or control?)
-        demog = np.array(self.file["demo"])
-        demog[:, 0] = np.nan_to_num(demog[:, 0])
-        demog[:, 0] = (demog[:, 0] - demog[:, 0].mean()) / demog[:, 0].std()
-        # array of patient demographic data
-        # first column is binary (sex) and second column is
-        # normalized age at time of last record
-        self.demo = torch.from_numpy(demog).float()
         # bin ages to nearest multiple of q
         self._age_q = age_quantum
         self.mask = True
@@ -76,7 +68,7 @@ class ClaimsDataset(Dataset):
             labels = torch.tensor(labels)
 
         # return array of diag codes and patient labels
-        return ages, visits, self.demo[index], patient_records, labels
+        return ages, visits, None, patient_records, labels
 
 
 class DiagnosisDataset(ClaimsDataset):
@@ -88,6 +80,14 @@ class DiagnosisDataset(ClaimsDataset):
         filename="preprocessed",
     ):
         super().__init__(preprocess_dir, filename)
+        # array of binary labels (is a patient a case or control?)
+        demog = np.array(self.file["demo"])
+        demog[:, 0] = np.nan_to_num(demog[:, 0])
+        demog[:, 0] = (demog[:, 0] - demog[:, 0].mean()) / demog[:, 0].std()
+        # array of patient demographic data
+        # first column is binary (sex) and second column is
+        # normalized age at time of last record
+        self.demo = torch.from_numpy(demog).float()
         self._bow = bag_of_words
         if not bag_of_words:
             self.records = torch.from_numpy(self.records)
@@ -98,7 +98,7 @@ class DiagnosisDataset(ClaimsDataset):
         self.mask = False
 
     def __getitem__(self, index):
-        ages, visits, self.demo[index], patient_records, _ = super().__getitem__(index)
+        ages, visits, _, patient_records, _ = super().__getitem__(index)
         if self._bow:
             patient_records = pad_bincount(patient_records, self.code_lookup.shape)
             ages = pad_bincount(ages, 100)
@@ -115,6 +115,14 @@ class EarlyDetectionDataset(ClaimsDataset):
         early_cutoff=90,
     ):
         super().__init__(preprocess_dir, filename)
+        # array of binary labels (is a patient a case or control?)
+        demog = np.array(self.file["demo"])
+        demog[:, 0] = np.nan_to_num(demog[:, 0])
+        demog[:, 0] = (demog[:, 0] - demog[:, 0].mean()) / demog[:, 0].std()
+        # array of patient demographic data
+        # first column is binary (sex) and second column is
+        # normalized age at time of last record
+        self.demo = torch.from_numpy(demog).float()
         self._bow = bag_of_words
         if not bag_of_words:
             self.records = torch.from_numpy(self.records)
@@ -206,6 +214,7 @@ def random_mask(patient_records, labels, n_tokens):
         prob = random.random()
         if prob < 0.15:
             prob /= 0.15
+            labels.append(patient_records[i])
             if prob < 0.8:
                 # mask out token
                 patient_records[i] = 1
@@ -213,7 +222,6 @@ def random_mask(patient_records, labels, n_tokens):
                 # replace with random token
                 patient_records[i] = random.randrange(n_tokens)
             # keep the same
-            labels.append(patient_records[i])
         else:
             labels.append(0)
 

tfmdoc/loader.py
@@ -15,9 +15,9 @@ def padded_collate(batch, pad=True, mode="pretraining"):
         ts, vs, ws, xs, ys = zip(*batch)
         if mode == "diagnosis":
             ys = torch.stack(ys)
+            ws = torch.stack(ws)
         elif mode == "pretraining":
             ys = pad_sequence(ys, batch_first=True, padding_value=0)
-    ws = torch.stack(ws)
     if pad:
         ts = pad_sequence(ts, batch_first=True, padding_value=0)
         vs = pad_sequence(vs, batch_first=True, padding_value=0)

tfmdoc/model.py
@@ -19,7 +19,6 @@ class Tfmd(pl.LightningModule):
         transformer,
         d_bow,
         lr,
-        mask=False,
     ):
         """Deep learning model for early detection of disease based on
             health insurance claims data. This model makes use of both
@@ -62,6 +61,7 @@ class Tfmd(pl.LightningModule):
             ]
             self._norm = torch.nn.LayerNorm(d_model)
             self._layers = torch.nn.Sequential(*blocks)
+            # incorporate demographic info in a  layer
             self._final = Linear(d_model + 2, d_ff)
         else:
             d_bow.append(d_model)
@@ -69,29 +69,22 @@ class Tfmd(pl.LightningModule):
             self.dense = torch.nn.Sequential(*bow_layers)
             self._final = Linear(d_bow[-1] + 2, d_ff)
         self.pr_curve = torchmetrics.PrecisionRecallCurve(pos_label=1)
-        # add two to input dim for demographic data (sex, age)
         self._to_scores = Linear(d_ff, 2)
-        self._loss_fn = torch.nn.CrossEntropyLoss()
+        self._loss_fn = torch.nn.CrossEntropyLoss(ignore_index=0)
         self._accuracy = torchmetrics.Accuracy()
         self._val_auroc = torchmetrics.AUROC(compute_on_step=False)
         self._test_auroc = torchmetrics.AUROC(compute_on_step=False)
         self._n_heads = n_heads
-        self._mask = mask
 
     def forward(self, ages, visits, demo, codes):
         # embed codes into dimension of model
         # for continuous representation
         if self.hparams.transformer:
-            if self._mask:
-                mask = (codes > 0).unsqueeze(1).repeat(1, codes.size(1), 1)
-                # very helpful documentation! (not)
-                mask = mask.repeat_interleave(repeats=self._n_head, dim=0)
-            else:
-                mask = None
             x = self.embed(codes)
             # apply position encodings
             x = self.pos_encode(visits, x)
             x = x + self.age_embed(ages)
+            mask = None
             for layer in self._layers:
                 x = layer(x, mask)
 